20bdfe07772ba98e0f9960eb6f6fb215a72de0d3
hyh <ec.huyinghuan@gmail.com>
2018-03-20 19:02:44 +0800

    init

--- index.go
+++ index.go
@@ -79,11 +79,7 @@ func (moment *Mymonent) LastPeriodDayUinx(day int) int64 {
 //Format  format date to string
 /*
 	YYYY	2014	4 or 2 digit year
-	YY	14	2 digit year
-	Y	-25	Year with any number of digits and sign
-	Q	1..4	Quarter of year. Sets month to first month in quarter.
 	M MM	1..12	Month number
-	MMM MMMM	Jan..December	Month name in locale set by moment.locale()
 	D DD	1..31	Day of month
 	Do	1st..31st	Day of month with ordinal
 	DDD DDDD	1..365	Day of year
